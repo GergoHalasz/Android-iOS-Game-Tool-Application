f5f361b92d9333418d81f4fe3141c864dc1e2752
Halasz Gergo <gergo.halasz@qubiz.ro>
2021-12-21 20:21:44 +0200

    fixed app rate bugs

--- lib/HomeScreen/load_home_screen.dart
+++ lib/HomeScreen/load_home_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
+import 'package:wowtalentcalculator/RateMyAppCustomClasses/minimum_app_launches_condition.dart';
 
+import '../RateMyAppCustomClasses/do_not_open_again_condition.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rate_my_app/rate_my_app.dart';
@@ -17,17 +19,33 @@ class _LoadHomeScreenState extends State<LoadHomeScreen> {
     preferencesPrefix: 'rateMyApp_',
     minDays: 0,
     minLaunches: 2,
-    remindDays: 7,
-    remindLaunches: 10,
+    remindDays: 0,
+    remindLaunches: 3,
     googlePlayIdentifier: 'com.fissher.wowtalentcalculator',
     appStoreIdentifier: '1593368066',
   );
+
   @override
   void initState() {
     WidgetsBinding.instance?.addPostFrameCallback((_) async {
+      CustomDoNotOpenAgainCondition condition = CustomDoNotOpenAgainCondition();
+      CustomMinimumAppLaunchesCondition minimumAppLaunchesCondition =
+          CustomMinimumAppLaunchesCondition(minLaunches: 2, remindLaunches: 5);
+      rateMyApp.conditions.add(condition);
+      rateMyApp.conditions.add(minimumAppLaunchesCondition);
       await rateMyApp.init();
       if (mounted && rateMyApp.shouldOpenDialog) {
-        rateMyApp.showRateDialog(context);
+        if (Platform.isAndroid) {
+          rateMyApp.showRateDialog(context,
+              title: 'Rate This App',
+              message: 'Do you like this app? Please leave a rating!',
+              actionsBuilder: actionsBuilderAndroid);
+        } else {
+          rateMyApp.showStarRateDialog(context,
+              title: 'Rate This App',
+              message: 'Do you like this app? Please leave a rating!',
+              actionsBuilder: actionsBuilderIOS);
+        }
       }
     });
     super.initState();
@@ -78,5 +96,25 @@ class _LoadHomeScreenState extends State<LoadHomeScreen> {
         },
       );
     }
+  }
+
+  List<Widget> actionsBuilderIOS(BuildContext context, double? stars) =>
+      stars == null
+          ? [buildOkButton()]
+          : [buildCancelButton(), buildRemindButton(), buildOkButton()];
+
+  List<Widget> actionsBuilderAndroid(BuildContext context) =>
+      [buildCancelButton(), buildRemindButton(), buildOkButton()];
+
+  Widget buildOkButton() {
+    return RateMyAppRateButton(rateMyApp, text: 'RATE');
+  }
+
+  Widget buildCancelButton() {
+    return RateMyAppNoButton(rateMyApp, text: 'CANCEL');
+  }
+
+  Widget buildRemindButton() {
+    return RateMyAppLaterButton(rateMyApp, text: 'LATER');
   }
 }
