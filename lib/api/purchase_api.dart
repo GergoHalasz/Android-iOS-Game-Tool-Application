2f18567ae73dee7c8bd53243b18e613f82e5c974
HalaszGergo123 <halasz.gergo2001@gmail.com>
2022-01-23 15:01:40 +0200

    bug fixed

--- lib/api/purchase_api.dart
+++ lib/api/purchase_api.dart
@@ -1,14 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 
 class PurchaseApi {
-  static const _apiKey = 'goog_zhICXognoNhhTiVmUMZLoyNhyXa';
-
   static Future init() async {
+    final apiKey = Platform.isIOS
+        ? 'appl_OFAINueZVlSYxIuLShwHKRDIHYb'
+        : 'goog_zhICXognoNhhTiVmUMZLoyNhyXa';
+
     await Purchases.setDebugLogsEnabled(true);
-    await Purchases.setup(_apiKey);
+    await Purchases.setup(apiKey);
   }
-  
+
   static Future<List<Offering>> fetchOffers() async {
     try {
       final offerings = await Purchases.getOfferings();
