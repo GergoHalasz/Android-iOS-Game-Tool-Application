a68f7f7f97c0d55f7345ba56063a0279ffd48fbe
HalaszGergo123 <halasz.gergo2001@gmail.com>
2024-03-29 13:28:07 +0200

    added api key

--- lib/api/purchase_api.dart
+++ lib/api/purchase_api.dart
@@ -5,20 +5,20 @@ import 'package:purchases_flutter/purchases_flutter.dart';
 
 class PurchaseApi {
   static Future init() async {
+    final apiKey = Platform.isIOS
+        ? 'appl_OFAINueZVlSYxIuLShwHKRDIHYb'
+        : 'goog_ORDoTnQCskoxBeCayVnpnAbFORY';
 
-    
-    // final apiKey = Platform.isIOS
-    //     ? 'appl_OFAINueZVlSYxIuLShwHKRDIHYb'
-    //     : 'goog_ORDoTnQCskoxBeCayVnpnAbFORY';
+    await Purchases.setDebugLogsEnabled(true);
+    PurchasesConfiguration configuration = PurchasesConfiguration(apiKey);
 
-    // await Purchases.setDebugLogsEnabled(true);
-    // await Purchases.setup(apiKey);
+    await Purchases.configure(configuration);
   }
 
   static Future<List<Offering>> fetchOffers() async {
     try {
       final offerings = await Purchases.getOfferings();
-      final current =   offerings.current;
+      final current = offerings.current;
 
       return current == null ? [] : [current];
     } on PlatformException {
