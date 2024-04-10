01d95daafa757f9e0248ecad164e9ddb9cff409b
HalaszGergo123 <halasz.gergo2001@gmail.com>
2024-04-10 11:19:03 +0300

    modfied UI for ios

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,11 +3,11 @@ const int NUM_ROWS = 6;
 const int NUM_COLS = 4;
 
 /// The padding of the Container of the [SpellsPositionedWidget].
-const double kTalentScreenPadding = 10.0;
+const double kTalentScreenPadding = 20.0;
 const double kTalentScreenTwoPadding = 40.0;
 
 /// The size of the Icon on Tab Bar of the [DetailScreen].
-const double kTabIconSize = 26.0;
+const double kTabIconSize = 32.0;
 
 /// The size of arrow [ArrowWidget].
 const double kArrowWidthSize = 20.0;
--- lib/utils/size_config.dart
+++ lib/utils/size_config.dart
@@ -1,43 +1,56 @@
 import 'package:flutter/widgets.dart';
 import 'package:wowtalentcalculator/utils/constants.dart';
 
-
 //https://medium.com/flutter-community/flutter-effectively-scale-ui-according-to-different-screen-sizes-2cb7c115ea0a
 
 class SizeConfig {
   static late MediaQueryData _mediaQueryData;
+
   /// the width of screen size
   static late double screenWidth;
+
   /// the height of screen size
   static late double screenHeight;
+
   /// the size of screen size
   static late Size screenSize;
+
   /// the actual width size after safeArea
   static late double safeAreaScreenWidth;
+
   /// the actual height size after safeArea
   static late double safeAreaScreenHeight;
+
   ///width divide by 100
   static late double blockSizeHorizontal;
+
   ///height divide by 100
   static late double blockSizeVertical;
+
   ///width of safeArea
   static late double _safeAreaHorizontal;
+
   ///height of safeArea
   static late double _safeAreaVertical;
+
   ///width after safeAre divide by 100
   static late double safeBlockHorizontal;
+
   ///height after safeAre divide by 100
   static late double safeBlockVertical;
+
   ///the width of icon cell of the [DetailScreen]
   static late double cellSize;
 
-
   void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
+
     /// the width of screen size
     screenWidth = _mediaQueryData.size.width;
+
     /// the height of screen size
     screenHeight = _mediaQueryData.size.height;
+
     /// the size of screen size
     screenSize = _mediaQueryData.size;
 
@@ -46,8 +59,10 @@ class SizeConfig {
     blockSizeVertical = screenHeight / 100;
 
     /// the width and height of safeArea
-    _safeAreaHorizontal = _mediaQueryData.padding.left + _mediaQueryData.padding.right;
-    _safeAreaVertical = _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
+    _safeAreaHorizontal =
+        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+    _safeAreaVertical =
+        _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
 
     /// divide safeArea screens into 100 equal blocks, used for calculation
     safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
@@ -58,7 +73,8 @@ class SizeConfig {
     safeAreaScreenHeight = screenHeight - _safeAreaVertical;
 
     /// the width of cell of the [DetailScreen], calculate upon init.
-    cellSize =screenWidth < 600 ?  (screenWidth / 1.2  - kTalentScreenTwoPadding) / NUM_COLS : 140;
-
+    cellSize = screenWidth < 600
+        ? ((screenWidth - kTalentScreenTwoPadding) / NUM_COLS / 1.3)
+        : 140;
   }
 }
